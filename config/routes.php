f50b12f6f401b76abb37a7e619ac5e9399ce91e7
Pachara Tinamas <mrpachara@gmail.com>
2020-03-27 15:40:49 +0700

    Group routes of product

--- config/routes.php
+++ config/routes.php
@@ -1,28 +1,32 @@
 <?php
 declare(strict_types = 1);
 
+use Slim\Routing\RouteCollectorProxy;
+
 use App\Controller\LoginController;
 use App\Controller\ProductController;
 
-$app->get('/product',
-  ProductController::class.':listAction'
-)->setName('product-list');
-
-$app->get('/product/add',
-  ProductController::class.':addFormAction'
-)->setName('product-add-form');
-
-$app->post('/product/add',
-  ProductController::class.':addAction'
-)->setName('product-add');
-
-$app->get('/product/{id}',
-  ProductController::class.':viewAction'
-)->setName('product-view');
-
-$app->get('/product/{id}/update',
-  ProductController::class.':updateFormAction'
-)->setName('product-update-form');
+$productGroup = $app->group('/product', function(RouteCollectorProxy $group) {
+  $group->get('',
+    ProductController::class.':listAction'
+  )->setName('product-list');
+  
+  $group->get('/add',
+    ProductController::class.':addFormAction'
+  )->setName('product-add-form');
+  
+  $group->post('/add',
+    ProductController::class.':addAction'
+  )->setName('product-add');
+  
+  $group->get('/{id}',
+    ProductController::class.':viewAction'
+  )->setName('product-view');
+  
+  $group->get('/{id}/update',
+    ProductController::class.':updateFormAction'
+  )->setName('product-update-form');
+});
 
 $app->get('/login',
   LoginController::class.':loginFormAction'
